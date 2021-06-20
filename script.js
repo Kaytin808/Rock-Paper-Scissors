c30d2c1847cbc253ce4f982aecb4fc2aabe5c44e
Kaytin808 <79622351+Kaytin808@users.noreply.github.com>
2021-06-20 15:39:00 -0700

    Added mouse hover and gib profile icon

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ var computerScore = 0;
 var userScore_span = document.getElementById("user-score");
 var computerScore_span = document.getElementById("computer-score");
 var scoreBoard_div = document.querySelector(".score-board");
-var results_div = document.querySelector(".results");
-var rock_div = document.getElementById("rock");
-var paper_div = document.getElementById("paper");
-var sciss_div = document.getElementById("scissors");
+var results_p = document.querySelector(".results > p");
+var rock_div = document.getElementById("Rock");
+var paper_div = document.getElementById("Paper");
+var sciss_div = document.getElementById("Scissors");
 // Pushes value into userChoice on click
 // randomizes rock paper scissors for computer
 function computerPlay() {
-  let word = ["rock", "paper", "scissors"];
+  let word = ["Rock", "Paper", "Scissors"];
   let thing = word[Math.floor(Math.random() * word.length)];
   return thing;
 }
@@ -19,26 +19,34 @@ computerPlay();
 // goes through userChoice after clicking choice //
 
 // onClick listen function //
-function tie() {
-  console.log("tie");
+function tie(userChoice, computerChoice) {
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  results_p.innerHTML = "It's a draw! Try again!😤😤😤";
 }
-function win() {
-  console.log("win");
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  results_p.innerHTML = `${userChoice} beats  ${computerChoice}, you win!💪😍`;
 }
-function loss() {
-  console.log("loss");
+function loss(userChoice, computerChoice) {
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  results_p.innerHTML =
+    computerChoice + " beats " + userChoice + ", you lost!💩💩💩";
 }
 function main() {
   rock_div.addEventListener("click", function () {
-    game("rock");
+    game("Rock");
   });
 
   paper_div.addEventListener("click", function () {
-    game("paper");
+    game("Paper");
   });
 
   sciss_div.addEventListener("click", function () {
-    game("scissors");
+    game("Scissors");
   });
 }
 main();
@@ -46,25 +54,20 @@ main();
 function game(userChoice) {
   const computerChoice = computerPlay();
   switch (userChoice + computerChoice) {
-    case "rockscissors":
-    case "scissorspaper":
-    case "paperrock":
-      userScore++;
-      console.log("Computer: " + computerScore, "User: " + userScore);
-      console.log("USER WINS");
+    case "RockScissors":
+    case "ScissorsPaper":
+    case "PaperRock":
+      win(userChoice, computerChoice);
       break;
-    case "scissorsrock":
-    case "paperscissors":
-    case "rockpaper":
-      computerScore++;
-      console.log("Computer: " + computerScore, "User: " + userScore);
-      console.log("USER LOSES!");
+    case "ScissorsRock":
+    case "PaperScissors":
+    case "RockPaper":
+      loss(userChoice, computerChoice);
       break;
-    case "rockrock":
-    case "paperpaper":
-    case "scissorsscissors":
-      console.log("Computer: " + computerScore, "User: " + userScore);
-      console.log("TIE");
+    case "RockRock":
+    case "PaperPaper":
+    case "ScissorsScissors":
+      tie(userChoice, computerChoice);
       break;
   }
 }
